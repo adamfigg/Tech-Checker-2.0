9daf5e1405ba211f6a253d2a119a53cc4516625c
Adam Figgat <afiggat@gmail.com>
2018-04-04 14:43:15 -0600

    started fresh on the tech checker

---
+++ my-app/src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import DB from './DB.js';
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      companyList: []
+    }
+    this.loadCompanies = this.loadCompanies.bind(this);
+  }
+
+  loadCompanies() {
+      this.setState({
+        companyList: DB.data,
+      }
+      )
+  }
+
+
+  render() {
+    return (
+
+      <div>
+        <h1>Welcome to Tech Checker</h1>
+
+        <button hidden={this.state.companyList.length > 0} onClick=
+          {this.loadCompanies} >Click for sweet companies</button>
+
+
+
+        <ul>
+          {this.state.companyList.map((company, i) => {
+            return <li> You could work at {company.name} if you know how to code with {company.techUsed} </li>
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ my-app/src/DB.js
@@ -0,0 +1,30 @@
+const DB = {  data: [
+    {
+        "name": "Apple",
+        "techUsed": [
+            "Swift",
+            "HTML",
+            "CSS"
+        ]
+    },
+    {
+        "name": "Google",
+        "techUsed": [
+            "CSS",
+            "HTML",
+            "Ruby",
+            "NodeJS"
+        ]
+    },
+    {
+        "name": "Tesla",
+        "techUsed": [
+            "React",
+            "Redux",
+            "RubyOnRails",
+            "HTML",
+            "CSS"
+        ]
+    }
+]};
+export default DB;
